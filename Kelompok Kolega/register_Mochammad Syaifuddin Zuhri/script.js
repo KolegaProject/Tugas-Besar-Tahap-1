d700b71926a905d25f9dca848baed46e1e6f6cff
Epulvis <zuhrisaifuddin405@gmail.com>
2025-11-01 19:15:35 +0700

    Fix : Link Referensi

--- Kelompok Kolega/register_Mochammad Syaifuddin Zuhri/script.js
+++ Kelompok Kolega/register_Mochammad Syaifuddin Zuhri/script.js
@@ -1,4 +1,13 @@
 document.getElementById("registerForm").addEventListener("submit", function (e) {
   e.preventDefault();
   alert("Akun berhasil dibuat!");
+  window.location.href = "../login_Muhammad Zhafran Ilham/index.html";
 });
+
+document.getElementById("googleBtn").addEventListener("click", function () {
+    window.location.href = "../login_Muhammad Zhafran Ilham/index.html";
+  });
+
+  document.getElementById("facebookBtn").addEventListener("click", function () {
+    window.location.href = "../login_Muhammad Zhafran Ilham/index.html";
+  });
